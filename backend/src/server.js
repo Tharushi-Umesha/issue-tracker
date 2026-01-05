748979f594feb6e2ecfebe1e0821ad6ca406244d
umesha <umemahee@gmai.com>
2026-01-05 11:09:57 +0530

    Add CORS configuration with FRONTEND_URL

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,13 @@ const issueRoutes = require('./routes/issueRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.FRONTEND_URL || '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
